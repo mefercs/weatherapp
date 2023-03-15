9d57437dcec1171dd616fd8e02fa850105580222
mefercs <mivki890@gmail.com>
2023-03-15 13:49:50 -0600

    finished the weather app json just decorate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
+  const [data, setData] = useState(null);
+  const [parse, setParse] = useState(true);
+
+  console.log("======== We are in a loop? ========");
+  useEffect(() => {
+    const getData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      await fetch(`${process.env.REACT_APP_WEATHER_URI}lat=${lat}&lon=${long}`)
+        .then((res) => res.json())
+        .then((result) => setData(result));
+    };
+    getData();
+  }, [lat, long]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data != null ? (
+        <div>
+          <p>Latitude: {lat}</p>
+          <p>Longitude: {long}</p>
+          <h1>JSON INFORMATION</h1>
+          <p> Your current temperature on your zone:
+            {!data.main
+              ? "loading..."
+              : parse
+              ? data.main.temp
+              : (data.main.temp * 1.8 + 32).toFixed(2)}{" "}
+            &deg;{parse ? "C" : "F"}
+          </p>
+          <p>{parse}</p>
+          <button onClick={() => setParse(p => !p)}>Farenheit</button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
