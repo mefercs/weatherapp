da1ec234f2f47730c63d9d1f5c7641ae63ac5ae8
mefercs <mivki890@gmail.com>
2023-03-16 19:45:01 -0600

    new homepage

--- src/App.js
+++ src/App.js
@@ -23,23 +23,25 @@ function App() {
   return (
     <div className="App">
       {data != null ? (
-        <div>
-          <p>Latitude: {lat}</p>
-          <p>Longitude: {long}</p>
-          <h1>JSON INFORMATION</h1>
-          <p> Your current temperature on your zone:
-            {!data.main
-              ? "loading..."
-              : parse
-              ? data.main.temp
-              : (data.main.temp * 1.8 + 32).toFixed(2)}{" "}
-            &deg;{parse ? "C" : "F"}
-          </p>
-          <p>{parse}</p>
-          <button onClick={() => setParse(p => !p)}>Farenheit</button>
+        <div className="card">
+            <p>Latitude: {lat}</p>
+            <p>Longitude: {long}</p>
+            <h1>JSON INFORMATION</h1>
+            <p>
+              {" "}
+              Your current temperature on your zone:
+              {!data.main
+                ? "loading..."
+                : parse
+                ? data.main.temp
+                : (data.main.temp * 1.8 + 32).toFixed(2)}{" "}
+              &deg;{parse ? "C" : "F"}
+            </p>
+            <p>{parse}</p>
+            <button onClick={() => setParse((p) => !p)}>{ parse?"Farenheit":"Celsius" }</button>
         </div>
       ) : (
-        <p>Loading...</p>
+        <div className="loading">Loading...</div>
       )}
     </div>
   );
